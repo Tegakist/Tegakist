d74bc5ae009830ac52b5bd672e2ceebd5d8011f1
Tegakist <today.is.another.day.157.6cm@gmail.com>
2024-08-05 14:43:04 +0900

    Update object-config.js

--- ARdev0C/object-config.js
+++ ARdev0C/object-config.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const box = document.createElement('a-box');
   box.setAttribute('position', '0 0 0');
   box.setAttribute('material', 'opacity: 0.5; color: red;');
-  box.setAttribute('visible', 'false');
+  box.setAttribute('visible', 'true'); // visibleをtrueに設定
+  box.setAttribute('id', 'box'); // idを追加
   marker.appendChild(box);
 });
