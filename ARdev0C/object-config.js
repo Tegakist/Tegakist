eda95015e3b2b3707c5182d3b508a1003a6a0bd7
Tegakist <today.is.another.day.157.6cm@gmail.com>
2024-08-05 17:56:08 +0900

    Update object-config.js

--- ARdev0C/object-config.js
+++ ARdev0C/object-config.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const marker = document.querySelector('a-marker');
-  const box = document.createElement('a-box');
-  box.setAttribute('position', '0 0 0');
-  box.setAttribute('material', 'opacity: 0.5; color: red;');
-  box.setAttribute('visible', 'true'); // visibleをtrueに設定
-  box.setAttribute('id', 'box'); // idを追加
-  marker.appendChild(box);
+    const marker = document.querySelector('a-marker');
+    const box = document.createElement('a-box');
+    box.setAttribute('position', '0 0 0');
+    box.setAttribute('material', 'opacity: 0.5; color: red;');
+    box.setAttribute('visible', 'true'); // visibleをtrueに設定
+    box.setAttribute('id', 'box'); // idを追加
+    box.setAttribute('persistent-visibility', ''); // persistent-visibilityを追加
+    marker.appendChild(box);
 });
