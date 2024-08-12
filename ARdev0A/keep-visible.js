e39f208c5181ddbb99a07f586c434461bf4bc393
Tegakist <today.is.another.day.157.6cm@gmail.com>
2024-08-12 15:56:38 +0900

    Delete ARdev0A/keep-visible.js

--- ARdev0A/keep-visible.js
+++
@@ -1,8 +0,0 @@
-AFRAME.registerComponent('keep-visible', {
-  tick: function () {
-    const box = document.querySelector('a-box');
-    if (!markerVisible && box) {
-      box.setAttribute('visible', 'true');
-    }
-  }
-});
