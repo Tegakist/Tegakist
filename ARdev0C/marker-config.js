d14773ff61f8fd17606afae07e74a65c35a8c087
Tegakist <today.is.another.day.157.6cm@gmail.com>
2024-08-16 10:53:34 +0900

    Delete ARdev0C/marker-config.js

--- ARdev0C/marker-config.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const marker = document.querySelector('a-marker');
-  marker.setAttribute('type', 'pattern');
-  marker.setAttribute('url', 'pattern-a.patt');
-  marker.setAttribute('emitevents', 'true');
-  marker.setAttribute('id', 'marker'); // idを追加
-});
