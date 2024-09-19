266a023c10bf25194aa4103c791945fff69e9e56
Tegakist <today.is.another.day.157.6cm@gmail.com>
2024-09-19 18:27:59 +0900

    Delete ARdev0F/camera_calibration.js

--- ARdev0F/camera_calibration.js
+++
@@ -1,9 +0,0 @@
-// カメラ行列
-const cameraMatrix = cv.matFromArray(3, 3, cv.CV_32FC1, [
-  1000, 0, 320,
-  0, 1000, 240,
-  0, 0, 1
-]);
-
-// 歪み係数
-const distCoeffs = cv.matFromArray(1, 5, cv.CV_32FC1, [0, 0, 0, 0, 0]);
